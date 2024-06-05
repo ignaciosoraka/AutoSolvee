6bf671a8ed3ffb15d6392c9eadd821d111b6b19c
ignaciosoraka <ignaciosoraka@gmail.com>
2024-06-05 15:48:27 -0300

    arreglos

--- correos.php
+++ correos.php
@@ -2,45 +2,62 @@
 $mensaje_exito = "";
 $mensaje_error = "";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Limpiar los datos del formulario
-    $nombre = htmlspecialchars($_POST['name']);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    $asunto = htmlspecialchars($_POST['subject']);
-    $empresa = htmlspecialchars($_POST['empresa']);
-    $mensaje = htmlspecialchars($_POST['message']);
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header("Location: index.html");
+    exit;
+}
 
-    // Personalizar el asunto del correo electrónico
-    $asuntoCorreo = "Consulta de " . $nombre;
+require 'phpmailer/PHPMailerAutoload.php';
 
-    // Construir el mensaje de correo electrónico
-    $mensajeCorreo = "Mensaje de contacto:\n\n";
-    $mensajeCorreo .= "Nombre: " . $nombre . "\n";
-    $mensajeCorreo .= "Correo Electrónico: " . $email . "\n";
-    $mensajeCorreo .= "Asunto: " . $asunto . "\n";
-    $mensajeCorreo .= "Empresa: " . $empresa . "\n";
-    $mensajeCorreo .= "Mensaje: " . $mensaje;
+$mail = new PHPMailer;
 
-    // Destinatario del correo electrónico
-    $destinatario = 'ignaciosoraka@gmail.com ';
+// Configuración SMTP
+$mail->isSMTP();
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPAuth = true;
+$mail->Username = 'soporte@kerneltech.dev';
+$mail->Password = 'hqyycwyzurppkzco';
+$mail->SMTPSecure = 'tls';
+$mail->Port = 587;
 
-    // Encabezados del correo electrónico
-    $headers = "From: Auto Solve <" . $email . ">\r\n";
-    $headers .= "Reply-To: " . $email . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+// Parámetros de envío
+$mail->setFrom('ignaciosoraka@gmail.com', 'AutoSolve');
+$mail->addAddress('ignaciosoraka@gmail.com', 'AutoSolve');
+$mail->isHTML(true);
 
-    // Enviar el correo electrónico y verificar si se envió correctamente
-    if (mail($destinatario, $asuntoCorreo, $mensajeCorreo, $headers)) {
-        // Éxito en el envío del correo electrónico
-        $mensaje_exito = "Tu mensaje ha sido enviado con éxito. ¡Gracias!";
-        // Redireccionar a gracias.html
-        header("Location: gracias.html");
-        exit; // Asegura que el script se detenga después de la redirección
-    } else {
-        // Error en el envío del correo electrónico
-        $mensaje_error = "Ocurrió un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
-    }
+// Limpiar los datos del formulario
+$nombre = isset($_POST['name']) ? htmlspecialchars($_POST['name']) : 'No especificado';
+$email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : 'No especificado';
+$asunto = isset($_POST['subject']) ? htmlspecialchars($_POST['subject']) : 'No especificado';
+$empresa = isset($_POST['empresa']) ? htmlspecialchars($_POST['empresa']) : 'No especificado';
+$mensaje = isset($_POST['message']) ? htmlspecialchars($_POST['message']) : 'No especificado';
+
+// Personalizar el asunto del correo electrónico
+$asuntoCorreo = "Consulta de " . $nombre;
+
+// Construir el mensaje de correo electrónico
+$mensajeCorreo = "<p>Mensaje de contacto:</p>";
+$mensajeCorreo .= "<p><strong>Nombre:</strong> " . $nombre . "</p>";
+$mensajeCorreo .= "<p><strong>Correo Electrónico:</strong> " . $email . "</p>";
+$mensajeCorreo .= "<p><strong>Asunto:</strong> " . $asunto . "</p>";
+$mensajeCorreo .= "<p><strong>Empresa:</strong> " . $empresa . "</p>";
+$mensajeCorreo .= "<p><strong>Mensaje:</strong><br>" . nl2br($mensaje) . "</p>";
+
+// Configurar el correo electrónico
+$mail->Subject = $asuntoCorreo;
+$mail->Body = $mensajeCorreo;
+$mail->AltBody = strip_tags($mensajeCorreo);
+
+// Enviar el correo electrónico y verificar si se envió correctamente
+if ($mail->send()) {
+    // Éxito en el envío del correo electrónico
+    $mensaje_exito = "Tu mensaje ha sido enviado con éxito. ¡Gracias!";
+    // Redireccionar a gracias.html
+    header("Location: gracias.html");
+    exit; // Asegura que el script se detenga después de la redirección
+} else {
+    // Error en el envío del correo electrónico
+    $mensaje_error = "Ocurrió un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde.";
+    echo "Mailer Error: " . $mail->ErrorInfo;
 }
 ?>
-
